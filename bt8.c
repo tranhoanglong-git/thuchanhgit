c2babc3585b93905b91eb7d2b527cfcce69f33d2
HuuTai <huahuutaiii.@gmail.com>
2025-10-21 09:43:40 +0700

    lan3

--- bt8.c
+++ bt8.c
@@ -10,4 +10,33 @@ int main() {
     }
     
     return 0;
-}ccccccccccccccccccccc
+}
+
+
+/* ===== HOẶC NGẮN HƠN NỮA ===== */
+
+// Cách 2: Dùng hàm
+void ve(int x) {
+    for (int i = 1; i <= x; i++) {
+        for (int j = 1; j <= i; j++)
+            printf("%d ", j);
+        printf("\n");
+    }
+}
+
+// Cách 3: Cực ngắn (1 dòng)
+void ve_1dong(int x) {
+    for (int i = 1; i <= x; i++, printf("\n"))
+        for (int j = 1; j <= i; j++)
+            printf("%d ", j);
+}
+
+// Cách 4: Nhập x từ bàn phím
+int main() {
+    int x, i, j;
+    scanf("%d", &x);
+    for (i = 1; i <= x; i++, printf("\n"))
+        for (j = 1; j <= i; j++)
+            printf("%d ", j);
+    return 0;
+}
