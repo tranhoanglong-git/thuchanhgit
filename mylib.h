c283a2212817dd725fe10d830735c7abefae24f9
Hoang Long <hoanglongtrannnnn@gmail.com>
2025-10-21 10:45:30 +0700

    Long ne con

---
+++ mylib.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void xemthang(); // bai 1
+void namnhuan(); // bai 2
+void giaiphuongtrinh(); // bai 3
+void nhapdiem(); // bai 4
+void tinhdien(); // bai 5
+void bangcuuchuong(); // bai 6
+void giaithua(); // bai 7
+void hienthi(); // bai 8
+void nhapdayso(); // bai 9
+void fibonacci(); // bai 10
+void inramanghinh(); // bai 11
